36a77296796c8889f3d2f0e48b78a8579a5cc913
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-21 15:50:20 +0200

    update friend dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,61 @@
 import React, { useState, useEffect } from "react";
 import { todoApi } from "../api/todoApi";
-import UserList from "../components/UserList";
+import { useAuth } from "../providers/auth";
 
 const Dashboard = () => {
-  const { get } = todoApi();
+  const { get, post } = todoApi();
   const [users, setUsers] = useState([]);
+  const { user: me } = useAuth();
 
   const getUsers = async () => {
     const resp = await get("/user/users");
     if (resp?.status === 200) {
       setUsers(resp.data);
     }
+  };
+  const getStatus = (user, i) => {
+    const friend = user.friends.find((friend) => friend.friendId === me.userId);
+    const status = friend?.friendStatus;
+
+    if (status === 0) return "Request sent";
+    if (status === 1) return "Request received";
+    if (status === 2) return "Friend";
+    return "Stranger";
+  };
+
+  const friendly = async (id) => {
+    await post("friend/add", { userId: id });
+    getUsers();
+  };
+
+  const unfriendly = async (id) => {
+    await post("friend/remove", { userId: id });
+    getUsers();
+  };
+
+  const usersData = (user, i) => {
+    const status = getStatus(user, i);
+
+    return (
+      <>
+        {user._id !== me.userId && (
+          <tr key={i}>
+            <td>{i + 1}</td>
+            <td>{user.username}</td>
+            <td> {user.played}</td>
+            <td> {user.won}</td>
+            <td> {user.won ? (user.won / user.played) * 100 : 0} </td>
+            <td>
+              <button onClick={() => friendly(user._id)}>:)</button>
+            </td>
+            <td>
+              <button onClick={() => unfriendly(user._id)}>:(</button>
+            </td>
+            <td>{status}</td>
+          </tr>
+        )}
+      </>
+    );
   };
 
   useEffect(() => {
@@ -34,9 +79,7 @@ const Dashboard = () => {
             <th>Friend Status</th>
           </tr>
         </thead>
-        <tbody>
-          {users && users.map((user, i) => <UserList key={i} user={user} />)}
-        </tbody>
+        <tbody>{users && users.map((user, i) => usersData(user, i))}</tbody>
       </table>
     </div>
   );
