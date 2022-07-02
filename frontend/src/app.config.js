92f2d25c407fe3dafb12b1bb7cdc848ab4803c00
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-07-02 14:16:42 +0200

    edit app config

--- frontend/src/app.config.js
+++ frontend/src/app.config.js
@@ -1,12 +1,16 @@
 const _config = {
   dev: {
-    api_url: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+    api_url:
+      process.env.REACT_APP_API_URL ||
+      "https://starfish-app-o5xid.ondigitalocean.app/api",
     google_client_id:
       "589986974868-mog8kd8qlf1lftigl8akeue7u3gj6hsv.apps.googleusercontent.com",
     google_base_url: "https://accounts.google.com/o/oauth2/v2/auth",
   },
   prod: {
-    api_url: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+    api_url:
+      process.env.REACT_APP_API_URL ||
+      "https://starfish-app-o5xid.ondigitalocean.app/api",
     google_client_id:
       process.env.REACT_APP_CLIENT_ID ||
       "589986974868-mog8kd8qlf1lftigl8akeue7u3gj6hsv.apps.googleusercontent.com",
